76a76efc2de149b2bb298d53cf396015a3d86e5d
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-08-25 17:49:35 -0500

    feat: :sparkles: Version 1.0.0 and 2.2.1

--- app/routes/incidencias.routes.js
+++ app/routes/incidencias.routes.js
@@ -1,18 +1,21 @@
 import { Router } from "express";
-import { postIncidencias,getIncidencias, putIncidencias } from "../controllers/incidencias.controller.js";
-import { body } from 'express-validator';
+import { postIncidencias,getIncidencias, putIncidencias } from "../controllers/V1/incidencias.controller.js";
+import routesVersioning from 'express-routes-versioning';
+import { get2Incidencias } from "../controllers/V2/incidencias.controller.js";
+import { validateToken } from "../middlewares/jtw.js";
+import { limitReq } from "../middlewares/limitReq.js";
 
 const appIncidencias = Router();
 
-//const version = routesVersioning();
+const version = routesVersioning();
+appIncidencias.use(validateToken, limitReq());
 
-appIncidencias.get('/',getIncidencias);
-appIncidencias.post('/', [
-    body('categoria')
-        .notEmpty().withMessage('La categoria es obligatoria')
-        .isString().withMessage('El dato debe ser de tipo string')
-        .matches(/^[a-z A-Z]+$/).withMessage('Solo acepta letras')
-], postIncidencias);
+
+appIncidencias.get('/',version({
+    "1.0.0": getIncidencias, postIncidencias, putIncidencias,
+    "2.2.1": get2Incidencias
+}));
+appIncidencias.post('/', postIncidencias);
 appIncidencias.put("/", putIncidencias);
 
 
