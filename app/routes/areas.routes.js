15c97d69e36343d0ecfa913535dcdaefc879cb5f
Jhoukx <jegonzalezesparragoza@gmail.com>
2023-08-25 20:27:12 -0500

    feat: :sparkles: add version to endpoint 'areas'

--- app/routes/areas.routes.js
+++ app/routes/areas.routes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { getAreas } from "../controllers/V1/areas.controller.js";
+import routesVersioning from 'express-routes-versioning'
+import { validateToken } from "../middlewares/jtw.js";
+import { limitReq } from "../middlewares/limitReq.js";
 
 const appAreas = Router();
+const version = routesVersioning();
 
-appAreas.get("/", getAreas);
+appAreas.use(validateToken,limitReq());
+
+appAreas.get("/", version({
+    "1.0.0": getAreas
+}));
 
 export default appAreas;
